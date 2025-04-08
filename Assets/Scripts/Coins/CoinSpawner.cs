48dd2767c0893c553d1e769590c59320470b4e10
pabloav57 <pabloav57@gmail.com>
2025-04-08 20:45:14 +0200

    2.15 Coin Spawner

---
+++ Assets/Scripts/Coins/CoinSpawner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CoinSpawner : NetworkBehaviour
+{
+    [SerializeField] private RespawningCoin coinPrefab;
+
+    [SerializeField] private int maxCoins = 50;
+    [SerializeField] private int coinValue = 10;
+    [SerializeField] private Vector2 xSpawnRange;
+    [SerializeField] private Vector2 ySpawnRange;
+    [SerializeField] private LayerMask layerMask;
+
+    private Collider2D[] coinBuffer = new Collider2D[1];
+    private float coinRadius;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer) { return; }
+
+        coinRadius = coinPrefab.GetComponent<CircleCollider2D>().radius;
+
+        for (int i = 0; i < maxCoins; i++)
+        {
+            SpawnCoin();
+        }
+    }
+
+    private void SpawnCoin()
+    {
+        RespawningCoin coinInstance = Instantiate(
+            coinPrefab,
+            GetSpawnPoint(),
+            Quaternion.identity);
+
+        coinInstance.SetValue(coinValue);
+        coinInstance.GetComponent<NetworkObject>().Spawn();
+
+        coinInstance.OnCollected += HandleCoinCollected;
+    }
+
+    private void HandleCoinCollected(RespawningCoin coin)
+    {
+        coin.transform.position = GetSpawnPoint();
+        coin.Reset();
+    }
+
+    private Vector2 GetSpawnPoint()
+    {
+    float x = 0;
+    float y = 0;
+    while (true)
+    {
+        x = Random.Range(xSpawnRange.x, xSpawnRange.y);
+        y = Random.Range(ySpawnRange.x, ySpawnRange.y);
+        Vector2 spawnPoint = new Vector2(x, y);
+
+        // Usar OverlapCircle y comprobar si el collider es null
+        Collider2D collider = Physics2D.OverlapCircle(spawnPoint, coinRadius, layerMask);
+        if (collider == null)
+        {
+            return spawnPoint;
+        }
+    }
+    }
+}
